4c7753d63b2c21b93005d054ec627d902722a87c
mplacona <marcos.placona@gmail.com>
2015-04-14 16:35:46 +0100

    Initial commit

---
+++ PasswordlessLogin/PhoneNumberPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PasswordlessLogin
+{
+	public class PhoneNumberPage : ContentPage
+	{
+		public PhoneNumberPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
---
+++ PasswordlessLogin/PhoneNumberPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PasswordlessLogin
+{
+	public partial class PhoneNumberPage : ContentPage
+	{
+		public PhoneNumberPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
